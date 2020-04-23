bed721d2ab93f8b2ee1aace04e580d32280f7cd1
Huan <530098379@qq.com>
2020-04-23 16:38:53 +0800

    添加到循环

--- main.py
+++ main.py
@@ -36,41 +36,46 @@ if __name__ == '__main__':
 	r =requests.post(url, data, cookies=cookie_jar)
 	print(r.status_code)
 
+	count=0
+	workbook = xlwt.Workbook()
+	sheet = workbook.add_sheet("Sheet Name1")
+
 	result = r.text
 	# 再次封装，获取具体标签内的内容
 	bs = BeautifulSoup(result,'html.parser')
 	# 获取已爬取内容中的Fiscal Year行的链接
 	data1=bs.select('a[class="getFormReportLink"]')
 	# 循环打印输出
-	#for j in data1:
-		#print(j['href'])
-
-	# 获取当前工会的Question 12
-	url = 'https://olms.dol-esa.gov/query/orgReport.do'
-	data = {'reportType':'formReport','detailID':'707004','detailReport':'LM2Form',
-			'rptView':'undefined','historyCount':'1','screenName':'orgDetailPage',
-			'searchPage':'/getOrgQry.do','pageAction':'-1','startRow':'1',
-			'endRow':'25','rowCount':'25','sortColumn':'','sortAscending':'false',
-			'reportTypeSave':'detailResults'}
-	r =requests.post(url, data, cookies=cookie_jar)
-	print(r.status_code)
-
-	result = r.text
-	# 再次封装，获取具体标签内的内容
-	bs = BeautifulSoup(result,'html.parser')
-	# 获取已爬取内容中的Fiscal Year行的链接
-	data1=bs.select('div[class="ERDS-form-text"] br')
-
-	count=0
-	workbook = xlwt.Workbook()
-	sheet = workbook.add_sheet("Sheet Name1")
-	# 循环打印输出
 	for j in data1:
-		# 判断下一个元素是字符串
-		if isinstance(j.next, str):
-			if re.match("Question\s12", j.next):
-				print(j.next)
-				sheet.write(count,0, "1") # row, column, value
-				sheet.write(count,1, "2019")
-				sheet.write(count,2, j.next)
+		year = (j.text)[0:4]
+		if year.isdigit() and int(year) > 2005 and (j.text).find('Report') >= 0:
+			print(j.text)
+			print(j['href'])
+
+			# 获取当前工会的Question 12
+			url = 'https://olms.dol-esa.gov/query/orgReport.do'
+			data = {'reportType':'formReport','detailID':'707004','detailReport':'LM2Form',
+					'rptView':'undefined','historyCount':'1','screenName':'orgDetailPage',
+					'searchPage':'/getOrgQry.do','pageAction':'-1','startRow':'1',
+					'endRow':'25','rowCount':'25','sortColumn':'','sortAscending':'false',
+					'reportTypeSave':'detailResults'}
+			r =requests.post(url, data, cookies=cookie_jar)
+			print(r.status_code)
+
+			result = r.text
+			# 再次封装，获取具体标签内的内容
+			bs = BeautifulSoup(result,'html.parser')
+			# 获取已爬取内容中的Fiscal Year行的链接
+			data1=bs.select('div[class="ERDS-form-text"] br')
+
+			# 循环打印输出
+			for j in data1:
+				# 判断下一个元素是字符串
+				if isinstance(j.next, str):
+					if re.match("Question\s12", j.next):
+						print(j.next)
+						sheet.write(count,0, "1") # row, column, value
+						sheet.write(count,1, "2019")
+						sheet.write(count,2, j.next)
+						count = count + 1;
 	workbook.save(os.getcwd() + '/result.xls')
