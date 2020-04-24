a17534b4db8dd6945caee10cdb9434fbd5cefb6e
Huan <530098379@qq.com>
2020-04-24 14:33:26 +0800

    在mac上可执行

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 from bs4 import BeautifulSoup
 import io
@@ -11,72 +13,81 @@ import time
 if __name__ == "__main__":
 	sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 	print("开始", flush = True)
-	# 获取cookie
-	url = "https://olms.dol-esa.gov/query/getOrgQry.do"
-	r =requests.get(url)
-	cookie_jar = r.cookies
 
+	# 做成Excel文件
 	count=0
 	workbook = xlwt.Workbook()
 	sheet = workbook.add_sheet("Sheet Name1")
 
+	# 读取文件里面的工会代码
 	excel_data = xlrd.open_workbook("lm2_auditor2.xls")
 	table = excel_data.sheet_by_index(0)
 
 	for rowNum in range(table.nrows):
 		rowVale = table.row_values(rowNum)
-		file_num = int(rowVale[0])
+		file_num = int(rowVale[0]) # 获取Excel第一列，工会代码
+
+		# 获取cookie
+		url_cok = "https://olms.dol-esa.gov/query/getOrgQry.do"
+		r_cok =requests.get(url_cok)
+		cookie_jar = r_cok.cookies
+
 		# 获取当前工会的所有年报
-		url = "https://olms.dol-esa.gov/query/orgReport.do"
-		data = {"reportType":"detailResults","detailID":file_num,"detailReport":"unionDetail",
+		url_union = "https://olms.dol-esa.gov/query/orgReport.do"
+		data_union = {"reportType":"detailResults","detailID":file_num,"detailReport":"unionDetail",
 				"rptView":"undefined","historyCount":"0","screenName":"orgQueryResultsPage",
 				"searchPage":"/getOrgQry.do","pageAction":"-1","startRow":"1",
 				"endRow":"1","rowCount":"1","sortColumn":"","sortAscending":"false",
 				"reportTypeSave":"orgResults"}
-		r =requests.post(url, data, cookies=cookie_jar)
+		r_union =requests.post(url_union, data_union, cookies=cookie_jar)
 		#print(r.status_code)
 
-		result = r.text
 		# 再次封装，获取具体标签内的内容
-		bs = BeautifulSoup(result,"html.parser")
+		result_union = r_union.text
+		bs_union = BeautifulSoup(result_union,"html.parser")
+
 		# 获取已爬取内容中的Fiscal Year行的链接
-		data1=bs.select("a[class='getFormReportLink']")
+		data_union = bs_union.select("a[class='getFormReportLink']")
+
 		# 循环打印输出
-		for j in data1:
+		for j in data_union:
 			year = (j.text)[0:4]
+			# 获取的链接，年份大于2005，并且是报告的时候，打开链接
 			if year.isdigit() and int(year) > 2005 and (j.text).find("Report") >= 0:
-				strlist = j["href"].split(",")
+				strlist = j["href"].split(",") # 获取年报的编码
 
 				# 获取当前工会的Question 12
-				url = "https://olms.dol-esa.gov/query/orgReport.do"
-				data = {"reportType":"formReport","detailID":strlist[1],"detailReport":"LM2Form",
+				url_detail = "https://olms.dol-esa.gov/query/orgReport.do"
+				data_detail = {"reportType":"formReport","detailID":strlist[1],"detailReport":"LM2Form",
 						"rptView":"undefined","historyCount":"1","screenName":"orgDetailPage",
 						"searchPage":"/getOrgQry.do","pageAction":"-1","startRow":"1",
 						"endRow":"25","rowCount":"25","sortColumn":"","sortAscending":"false",
 						"reportTypeSave":"detailResults"}
-				r =requests.post(url, data, cookies=cookie_jar)
+				r_detail =requests.post(url_detail, data_detail, cookies=cookie_jar)
 				#print(r.status_code)
 
-				result = r.text
 				# 再次封装，获取具体标签内的内容
-				bs = BeautifulSoup(result,"html.parser")
+				result_detail = r_detail.text
+				bs_detail = BeautifulSoup(result_detail,"html.parser")
+
 				# 获取已爬取内容中的Fiscal Year行的链接
-				data1=bs.select("div[class='ERDS-form-text'] br")
+				data_detail = bs_detail.select("div[class='ERDS-form-text'] br")
 
 				# 循环打印输出
-				for j in data1:
+				for k in data_detail:
 					# 判断下一个元素是字符串
-					if isinstance(j.next, str):
-						if re.match("Question\s12", j.next):
+					if isinstance(k.next, str):
+						if re.match("Question\s12", k.next):
 							print("工会编号:" + str(file_num), flush = True)
 							print("年份:" + year, flush = True)
-							print("内容:" + j.next, flush = True)
+							print("内容:" + k.next, flush = True)
 							print("--------------------------")
 							sheet.write(count,0, file_num) # row, column, value
 							sheet.write(count,1, year)
-							sheet.write(count,2, j.next)
+							sheet.write(count,2, k.next)
 							count = count + 1;
+			# 延迟5秒，防止访问太快
 			time.sleep(5)
-
+	# 输出结果到Excel
 	workbook.save(os.getcwd() + "/result.xls")
 	print("完成",flush = True)
